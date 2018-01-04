ae28f7d2ee68c9c32e028ec81518a7e554d78be7
RaicarvalhoRCO <35072872+RaicarvalhoRCO@users.noreply.github.com>
2018-01-04 14:16:31 -0200

    Pular por baixo das plataformas

---
+++ Assets/Scripts/PlatCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatCollider : MonoBehaviour {
+
+
+    public GameObject Platform;
+
+    void Start ()
+    {
+        Platform.GetComponent<Collider2D>().enabled = false;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Platform.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
