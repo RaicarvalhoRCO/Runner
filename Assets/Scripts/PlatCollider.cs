f8dc4f0c0fca1066eabbb1eb39190cdb325855cf
RaicarvalhoRCO <35072872+RaicarvalhoRCO@users.noreply.github.com>
2018-01-05 15:43:51 -0200

    Base do sistema de lanes

--- Assets/Scripts/PlatCollider.cs
+++ Assets/Scripts/PlatCollider.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class PlatCollider : MonoBehaviour {
 
 
-    public GameObject Platform;
+    public Collider2D Platform;
 
     void Start ()
     {
-        Platform.GetComponent<Collider2D>().enabled = false;
+        Platform.enabled = false;
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
-            Platform.GetComponent<Collider2D>().enabled = true;
+            Platform.enabled = true;
         }
     }
 }
