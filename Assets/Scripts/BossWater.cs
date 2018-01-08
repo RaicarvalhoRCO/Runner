5d35e0d0ed345e92b423e2c25647836d739b7952
RaicarvalhoRCO <35072872+RaicarvalhoRCO@users.noreply.github.com>
2018-01-08 16:16:34 -0200

    Inicio boss água

---
+++ Assets/Scripts/BossWater.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossWater : MonoBehaviour
+{
+    private player player;
+    private Animator anim;
+    private GameObject clone;
+    private Transform mob;
+    private Transform spawntiro;
+    public GameObject tiro;
+    private Rigidbody2D rbmob;
+
+
+    private float drop;
+    public float shootingrate;
+    private float shootcd = 0f;
+    public float vida = 5;
+    private bool death = false;
+
+
+    void Start()
+    {
+
+        rbmob = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spawntiro = GetComponent<Transform>();
+        mob = GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        if (shootcd > 0)
+        {
+            shootcd -= Time.deltaTime;
+        }
+        if (vida < 6)
+        {
+            anim.SetBool("shieldanim", false);
+        }
+        Fire();
+    }
+
+    void Fire()
+    {
+        if (shootcd <= 0f)
+        {
+            var clone = Instantiate(tiro, spawntiro.position, Quaternion.identity) as GameObject;
+            clone.transform.localScale = this.transform.localScale;
+            shootcd = shootingrate;
+        }
+    }
+
+
+    void spawn()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "tiro")
+        {
+            vida--;
+            if (vida == 0)
+            {
+                anim.SetBool("death", true);
+                Destroy(gameObject, 1f);
+                spawn();
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    private Transform spawntiro;
+    public GameObject bubbles;
+    public GameObject target;
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float shootcd = 2f;
+    private int cd = 0;
+    public float shootingrate;
+    private float x = 0;
+    private float y = 1;
+
+    void Start()
+    {
+        spawntiro = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion qt = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, qt, Time.deltaTime * rotationSpeed);
+        shootcd -= Time.deltaTime;
+
+        if (shootcd == 0f)
+        {
+            while(cd<9)
+            {
+                x += 0.2f;
+                y -= 0.2f;
+                var Direction = Vector3(x, y, 0f);
+                var clone = Instantiate(bubbles, Direction, Quaternion.identity) as GameObject;
+
+                cd++;
+            }
+            
+            shootcd = shootingrate;
+        }
+    }
+}
