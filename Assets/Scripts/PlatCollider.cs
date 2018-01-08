8e3a6f04725be9ffc0d44c8e22a5ce63e9373b13
RaicarvalhoRCO <35072872+RaicarvalhoRCO@users.noreply.github.com>
2018-01-08 16:16:47 -0200

    correção plataformas

--- Assets/Scripts/PlatCollider.cs
+++ Assets/Scripts/PlatCollider.cs
@@ -6,10 +6,23 @@ public class PlatCollider : MonoBehaviour {
 
 
     public Collider2D Platform;
+    private Collider2D Platformactive;
+    public float downtime = 0.5f;
 
     void Start ()
     {
+        Platformactive = GetComponent<Collider2D>();
         Platform.enabled = false;
+        Platformactive.enabled = true;
+    }
+
+    private void Update()
+    {
+        downtime -= Time.deltaTime;
+        if(downtime <=0)
+        {
+            Platformactive.enabled = true;
+        }
     }
 
     void OnTriggerStay2D(Collider2D col)
@@ -17,6 +30,21 @@ public class PlatCollider : MonoBehaviour {
         if (col.gameObject.tag == "Player")
         {
             Platform.enabled = true;
+            Platformactive.enabled = false;
+            
         }
+       
     }
+
+    public void Downcol()
+    {
+        if(Platform.enabled)
+        {
+            Platform.enabled = false;
+            downtime = 0.5f;
+            
+        }
+       
+    }
+
 }
